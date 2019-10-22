9360e692c86949840ded5fcfc803a3e67ba75c8d
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2019-10-22 11:22:47 +0200

    funcionalidad?

--- app/src/main/java/com/example/gridlayoutpresentacion/MainActivity.java
+++ app/src/main/java/com/example/gridlayoutpresentacion/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.gridlayoutpresentacion;
 
+import androidx.annotation.IntegerRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +21,32 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void verValor(View v){
+
         //Obtenemos el valor del edit text
         EditText campoText = (EditText) findViewById(R.id.teclado);
+        TextView titulo = (TextView) findViewById(R.id.titulo);
+        CalendarView calendario = (CalendarView) findViewById(R.id.calendario);
+
+        String date = "29/8/1996";
+        String parts[] = date.split("/");
+
+        int day = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int year = Integer.parseInt(parts[2]);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, (month - 1));
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        long milliTime = calendar.getTimeInMillis();
+
+        if(campoText.getText().toString().equals("Miguel")){
+            calendario.setDate( milliTime,true,true);
+            campoText.setText("");
+            //campoText.setText("");
+        }
+
     }
 }
 
