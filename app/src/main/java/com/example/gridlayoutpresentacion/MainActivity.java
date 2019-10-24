79fabf8e2f2e5c9f3817aaab5990a30d6b5de5fd
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2019-10-24 23:33:56 +0200

    menu opciones

--- app/src/main/java/com/example/gridlayoutpresentacion/MainActivity.java
+++ app/src/main/java/com/example/gridlayoutpresentacion/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -151,5 +153,28 @@ public class MainActivity extends AppCompatActivity {
         }
         
     }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu_en_activity,menu);
+
+        return true;
+
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem opcionMenu){
+
+        int id = opcionMenu.getItemId();
+
+        if(id == R.id.borrar){
+            //llevar a la actividad de borrar
+            ir(null);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(opcionMenu);
+
+    }
+
 }
 
